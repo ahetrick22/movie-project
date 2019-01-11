cd49a5159e4bc15ca23e63482df81dc71a766815
Alicia Hetrick <ahetrick22@gmail.com>
2019-01-11 15:32:44 -0500

    ready to deploy

---
+++ server/client/build/precache-manifest.98e59b5ac453a1896c34f7fa142df565.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "03cac4e91cd3621b77b7",
+    "url": "/static/js/main.03cac4e9.chunk.js"
+  },
+  {
+    "revision": "90611bd168aa6c4ca1cc",
+    "url": "/static/js/1.90611bd1.chunk.js"
+  },
+  {
+    "revision": "03cac4e91cd3621b77b7",
+    "url": "/static/css/main.9ae25004.chunk.css"
+  },
+  {
+    "revision": "9a5fd71b89e86721c93d5e744b484779",
+    "url": "/index.html"
+  }
+];
--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,4 @@
-// prod.js - production keys here!!
 module.exports = {
-  MONGODB_URI: process.env.MONGODB_URI,
-  TOKEN_SECRET: process.env.TOKEN_SECRET
-};
+    MONGODB_URI: process.env.MONGO_URI,
+    TOKEN_SECRET: process.env.COOKIE_KEY
+  };
